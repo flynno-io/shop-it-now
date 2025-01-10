469e441623bcbd2bb81a002d0d30819fed7a173f
flynno <luke.laz.flynn@gmail.com>
2025-01-10 15:06:29 -0700

    updated cart state to Redux -> cartReducer

--- client/src/utils/reducers/cartReducer.js
+++ client/src/utils/reducers/cartReducer.js
@@ -1,51 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit"
+
+// Create a cartAdapter
+const cartAdapter = createEntityAdapter({
+  // Tells the adapter how to select the id property of cart items.
+  // The _id property is the unique identifier for each cart item
+  selectId: (cartItem) => cartItem._id,
+})
 
 // The initial state of the application is an object with the following properties.
-const initialState = {
-  products: [],
-  cart: [],
-  cartOpen: false,
-};
+const initialState = cartAdapter.getInitialState({
+	cartOpen: false,
+})
 
 const cartReducer = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
-    addToCard(state, action) {
-      state.cart.push(action.payload);
-      state.cart.cartOpen = true;
-    },
-    addMutlipleToCart(state, action) {
-      state.cart.push(...action.payload)
-    },
+    addToCart: cartAdapter.addOne,
+    addMutlipleToCart: cartAdapter.addMany,
     updateCartQuantity(state, action) {
-      state.cart.cartOpen = true;
-      state.cart = state.cart.map((product) => {
-        if (action.payload._id === product._id) {
-          product.purchaseQuantity = action.payload.purchaseQuantity;
-        }
-        return product;
-      });
+      cartAdapter.updateOne(state, {
+        id: action.payload._id,
+        changes: { purchaseQuantity: action.payload.purchaseQuantity },
+      })
+      state.cartOpen = true
     },
     removeFromCart(state, action) {
-      let newState = state.cart.filter((product) => {
-        return product._id !== action.payload;
-      });
-      state.cart = newState;
-      state.cartOpen = newState.length > 0;
+      cartAdapter.removeOne(state, action.payload._id)
+      state.cartOpen = state.ids.length > 0
     },
-    clearCard(state, action) {
-      state.cart = [];
-      state.cartOpen = false;
+    clearCart(state) {
+      cartAdapter.removeAll(state)
+      state.cartOpen = false
     },
-    toggleCart(state, action) {
-      state.cartOpen = !state.cartOpen;
+    toggleCart(state) {
+      state.cartOpen = !state.cartOpen
     },
-  }
+  },
 })
 
 // Export the reducer functions as actions to be called in the application.
-export const { addToCard, addMutlipleToCart, updateCartQuantity, removeFromCart, clearCard, toggleCart } = cartReducer.actions;
+export const {
+	addToCart,
+	addMutlipleToCart,
+	updateCartQuantity,
+	removeFromCart,
+	clearCart,
+	toggleCart,
+} = cartReducer.actions
+
+// Export the selectors to access the state data.
+export const {
+  selectAll: selectAllCartItems,
+} = cartAdapter.getSelectors((state) => state.cart)
 
 // Export the reducer as the default.
-export default cartReducer.reducer;
+export default cartReducer.reducer
