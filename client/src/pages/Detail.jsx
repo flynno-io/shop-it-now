f6256b89f78e6c13c769cf51db800810db495be1
flynno <luke.laz.flynn@gmail.com>
2025-01-10 15:49:03 -0700

    updated Details to use Redux

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,127 +1,117 @@
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-
-import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import { useQuery } from "@apollo/client"
+import Cart from "../components/Cart"
+import { QUERY_PRODUCTS } from "../utils/queries"
+import { idbPromise } from "../utils/helpers"
+import spinner from "../assets/spinner.gif"
+import { useDispatch, useSelector } from "react-redux"
 import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from '../utils/actions';
-import { QUERY_PRODUCTS } from '../utils/queries';
-import { idbPromise } from '../utils/helpers';
-import spinner from '../assets/spinner.gif';
+	removeFromCart,
+	updateCartQuantity,
+	addToCart,
+	selectAllCartItems,
+} from "../utils/reducers/cartReducer"
+import {
+	updateProducts,
+	selectAllProducts,
+} from "../utils/reducers/productReducer"
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
-  const { id } = useParams();
+	const dispatch = useDispatch()
+	const products = useSelector(selectAllProducts)
+	const cart = useSelector(selectAllCartItems)
 
-  const [currentProduct, setCurrentProduct] = useState({});
+	const { id } = useParams()
+	const [currentProduct, setCurrentProduct] = useState({})
+	const { loading, data } = useQuery(QUERY_PRODUCTS)
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+	useEffect(() => {
+		// already in global store
+		if (products.length) {
+			const product = products.find((product) => product._id === id)
 
-  const { products, cart } = state;
+			const item = {
+				image: product.image,
+				name: product.name,
+				_id: product._id,
+				price: product.price,
+				quantity: product.quantity,
+			}
 
-  useEffect(() => {
-    // already in global store
-    if (products.length) {
-      const product = products.find((product) => product._id === id);
+			setCurrentProduct(item)
+		}
+		// retrieved from server
+		else if (data) {
+			dispatch(updateProducts({ ...data.products }))
 
-      const item = {
-        image: product.image,
-        name: product.name,
-        _id: product._id,
-        price: product.price,
-        quantity: product.quantity,
-      };
-      
-      setCurrentProduct(item);
-    }
-    // retrieved from server
-    else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
+			data.products.forEach((product) => {
+				idbPromise("products", "put", product)
+			})
+		}
+		// get cache from idb
+		else if (!loading) {
+			idbPromise("products", "get").then((indexedProducts) => {
+				dispatch(updateProducts({ ...indexedProducts }))
+			})
+		}
+	}, [products, data, loading, dispatch, id])
 
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-    }
-    // get cache from idb
-    else if (!loading) {
-      idbPromise('products', 'get').then((indexedProducts) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: indexedProducts,
-        });
-      });
-    }
-  }, [products, data, loading, dispatch, id]);
+	const addItemToCart = () => {
+		const itemInCart = cart.find((cartItem) => cartItem._id === id)
+		if (itemInCart) {
+			dispatch(
+				updateCartQuantity({
+					_id: id,
+					purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+				})
+			)
+			idbPromise("cart", "put", {
+				...itemInCart,
+				purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+			})
+		} else {
+			dispatch(addToCart({ ...currentProduct, purchaseQuantity: 1 }))
+			idbPromise("cart", "put", { ...currentProduct, purchaseQuantity: 1 })
+		}
+	}
 
-  const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === id);
-    if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
-      idbPromise('cart', 'put', {
-        ...itemInCart,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
-    } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...currentProduct, purchaseQuantity: 1 },
-      });
-      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
-    }
-  };
+	const removeItemFromCart = () => {
+		dispatch(removeFromCart( { _id: currentProduct._id }))
+		idbPromise("cart", "delete", { ...currentProduct })
+	}
 
-  const removeFromCart = () => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: currentProduct._id,
-    });
+	return (
+		<>
+			{currentProduct && cart ? (
+				<div className="container my-1">
+					<Link to="/">← Back to Products</Link>
 
-    idbPromise('cart', 'delete', { ...currentProduct });
-  };
+					<h2>{currentProduct.name}</h2>
 
-  return (
-    <>
-      {currentProduct && cart ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+					<p>{currentProduct.description}</p>
 
-          <h2>{currentProduct.name}</h2>
+					<p>
+						<strong>Price:</strong>${currentProduct.price}{" "}
+						<button onClick={addItemToCart}>Add to Cart</button>
+						<button
+							disabled={!cart.find((p) => p._id === currentProduct._id)}
+							onClick={removeItemFromCart}
+						>
+							Remove from Cart
+						</button>
+					</p>
 
-          <p>{currentProduct.description}</p>
-
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button onClick={addToCart}>Add to Cart</button>
-            <button
-              disabled={!cart.find((p) => p._id === currentProduct._id)}
-              onClick={removeFromCart}
-            >
-              Remove from Cart
-            </button>
-          </p>
-
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-      <Cart />
-    </>
-  );
+					<img
+						src={`/images/${currentProduct.image}`}
+						alt={currentProduct.name}
+					/>
+				</div>
+			) : null}
+			{loading ? <img src={spinner} alt="loading" /> : null}
+			<Cart />
+		</>
+	)
 }
 
-export default Detail;
+export default Detail
