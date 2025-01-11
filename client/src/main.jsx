d665db049d12e62b38e53a5ac8714428e2c9d008
flynno <luke.laz.flynn@gmail.com>
2025-01-10 22:20:47 -0700

    fixed 404 Page (NoMatch) and added "return to home" link

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    error: <NoMatch />,
+    errorElement: <NoMatch />,
     children: [
       {
         index: true, 
--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -1,18 +1,22 @@
-import Jumbotron from "../components/Jumbotron";
+import Jumbotron from "../components/Jumbotron"
+import { Link } from "react-router-dom"
 
 const NoMatch = () => {
-  return (
-    <div>
-      <Jumbotron>
-        <h1>404 Page Not Found</h1>
-        <h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄
-          </span>
-        </h1>
-      </Jumbotron>
-    </div>
-  );
-};
+	return (
+		<div>
+			<Jumbotron>
+				<h1>404 Page Not Found</h1>
+				<h1>
+					<span role="img" aria-label="Face With Rolling Eyes Emoji">
+						🙄
+					</span>
+				</h1>
+        <Link to="/" style={{ color: "blue", textDecoration: "underline" }}>
+						Go back to Home
+					</Link>
+			</Jumbotron>
+		</div>
+	)
+}
 
-export default NoMatch;
+export default NoMatch
