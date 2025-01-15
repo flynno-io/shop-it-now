a484dab85bbb2e1e780d4782cfd05e726332267f
flynno <luke.laz.flynn@gmail.com>
2025-01-14 20:19:24 -0700

    updated connection to use prod mongoDB Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping';
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+mongoose.connect(uri, clientOptions)
+  .then(() => {
+    console.log("Successfully connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB: ", err);
+  });
 
 module.exports = mongoose.connection;
